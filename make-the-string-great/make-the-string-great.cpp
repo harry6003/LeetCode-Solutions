681275fb9373896e5c5b2b4a71970658dc9ff378
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-08-19 21:07:17 +0530

    Time: 4 ms (62.67%), Space: 6.1 MB (93.56%) - LeetHub

--- make-the-string-great/make-the-string-great.cpp
+++ make-the-string-great/make-the-string-great.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string makeGood(string s, int p = 0) {
+string makeGood(string s, int p = 0) {
     for (int i = 0; i < s.size(); ++i) {
         if (p > 0 && abs(s[i] - s[p - 1]) == 32)
             --p;
