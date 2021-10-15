18337add0984a9c86aefb75dd696a6486f4219e0
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-10-15 11:04:37 +0530

    Time: 68 ms (60.03%), Space: 13.4 MB (33.85%) - LeetHub

---
+++ 4sum/4sum.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        set <vector<int>> s;
+        if(nums.size() < 3)
+            return ans;
+        
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<nums.size()-3;i++)
+        {
+            if(i == 0 || (i>0 && nums[i] != nums[i-1]))
+            {
+                 for(int j=i+1;j<nums.size()-2;j++)
+                 {
+                    if(j==(i+1) || nums[j]  != nums[j-1])
+                    {
+                        int low = j+1;
+                        int high = nums.size()-1;
+                        int sum = target - (nums[i] + nums[j]);
+                        
+                        while(low<high)
+                        {
+                           if(nums[low] + nums[high] == sum)
+                           {
+                                 vector <int> a;
+                                 a.push_back(nums[low]);
+                                 a.push_back(nums[high]);
+                                 a.push_back(nums[i]);
+                                 a.push_back(nums[j]);
+                                 sort(a.begin(),a.end());
+                                 s.insert(a);
+                                
+                               
+                               while(low<high && nums[low] == nums[low+1])
+                                   low++;
+                               while(low<high && nums[high] == nums[high-1])
+                                   high--;
+                               
+                               low++;
+                               high--;
+                           }
+                           else if(nums[low] + nums[high] < sum)
+                           {
+                               low++;
+                           }
+                           else 
+                           {
+                               high--;
+                           }
+                        }
+                    }
+                 }
+             
+            }
+        }
+        for(auto it = s.begin();it != s.end();it++)
+            ans.push_back(*it);
+        return ans;
+    }
+};
