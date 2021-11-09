6acce91cf41a0df23acc8c28903af7246123c10c
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-11-10 01:38:28 +0530

    Time: 124 ms (92.81%), Space: 86.1 MB (39.45%) - LeetHub

--- all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -1,7 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-	//helper funtion to in-order traverse a BST
-    void inOrderTraverse(TreeNode *root, vector<int>& vec)
+   void inOrderTraverse(TreeNode *root, vector<int>& vec)
     {
         if(root)
 	    {
@@ -11,8 +21,7 @@ public:
 
 	    }
     }
-	//helper function to merge two sorted vectors into one sorted vector
-    void mergeVectors(vector<int> vec1, vector<int> vec2, vector<int>& mergedVectors)
+  void mergeVectors(vector<int> vec1, vector<int> vec2, vector<int>& mergedVectors)
     {
         int i = 0, j = 0, k = 0; 
 
@@ -36,18 +45,16 @@ public:
         
         
     } 
-    
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2)
-    {
-        vector<int> tree1, tree2;
-		//in-order traverse both trees, now tree1 has 1st tree's elements sorted and tree2 has 2nd tree's elements sorted
-        inOrderTraverse(root1, tree1);
-        inOrderTraverse(root2, tree2);
+  
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+      vector<int> v1;
+      vector<int> v2;
+       inOrderTraverse(root1, v1);
+        inOrderTraverse(root2, v2);
         
-        vector<int> mergedVectors(tree1.size() + tree2.size());
-		//merge tree1 and tree2 into one merged sorted vector
-        mergeVectors(tree1, tree2, mergedVectors);
+     vector<int> mergedVectors(v1.size() + v2.size());
+		
+        mergeVectors(v1, v2, mergedVectors);
         return  mergedVectors;
-        
     }
 };
