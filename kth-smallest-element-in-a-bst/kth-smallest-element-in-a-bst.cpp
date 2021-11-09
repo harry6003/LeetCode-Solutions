39263b3cd0fc4888ce652e5e89101a1324583599
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-11-10 00:39:30 +0530

    Time: 20 ms (60.28%), Space: 24 MB (87.37%) - LeetHub

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,16 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-  void inorder(TreeNode* node, int& fid, int& k) {
-    if (!node) return;
-    inorder(node->left, fid, k);
-    if (!k) return;
-    fid = node->val;
-    inorder(node->right, fid, --k);
-}
-
-int kthSmallest(TreeNode* root, int k) {
-    int fid;
-    inorder(root, fid, k);
-    return fid;
-}
+    int kthSmallest(TreeNode* root, int k) {
+      if(root == NULL)
+          return -1;
+      int count = 0;
+      int kmin = INT_MAX;
+      TreeNode * curr = root;
+       TreeNode * pre;
+      while(curr)
+      {
+        if(curr->left==NULL)
+        {
+          count++;
+          if(k==count)
+          {
+            kmin= curr->val;
+          }
+          curr = curr->right;
+        }
+        else
+        {
+         pre = curr->left;
+          while(pre->right != NULL && pre->right != curr)
+          {
+            pre = pre->right;
+          }
+          
+          if(pre->right == NULL)
+          {
+            pre->right = curr;
+            curr = curr->left;
+          }
+          else
+          {
+            pre->right = NULL;
+            count++;
+            if(k == count)
+            {
+              kmin= curr->val;
+            }
+            curr = curr->right;
+          }
+        }
+      }
+      
+      return kmin;
+    }
 };
