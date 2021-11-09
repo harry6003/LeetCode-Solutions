32ddef3a45491dac157b4ef03ff4a3a878ab198e
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-11-09 13:25:52 +0530

    Time: 36 ms (82.93%), Space: 39 MB (28.10%) - LeetHub

--- two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,38 +1,12 @@
-class BSTIterator {
-    stack<TreeNode*> s;
-    TreeNode* node;
-    bool forward;
-    int cur;
-public:
-    BSTIterator(TreeNode *root, bool forward) : node(root), forward(forward) { (*this)++; };
-    int operator*() { return cur; }
-    void operator++(int) {
-        while (node || !s.empty()) {
-            if (node) {
-                s.push(node);
-                node = forward ? node->left : node->right;
-            }
-            else {
-                node = s.top();
-                s.pop();
-                cur = node->val;
-                node = forward ? node->right : node->left;
-                break;
-            }
-        }
-    }
-};
 class Solution {
 public:
+    unordered_set<int> s;
     bool findTarget(TreeNode* root, int k) {
-        if (!root) return false;
-        BSTIterator l(root, true);
-        BSTIterator r(root, false);
-        while (*l < *r) {
-            if (*l + *r == k) return true;
-            else if (*l + *r < k) l++;
-            else r++;
-        }
+        if(!root)
         return false;
+        if(s.count(k-root->val)) 
+        return true;
+        s.insert(root->val);
+        return findTarget(root->left,k)||findTarget(root->right,k);
     }
 };
