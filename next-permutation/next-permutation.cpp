95e3cda6addc774cb079b74ee7aed25aa34a04c4
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-12-01 21:05:38 +0530

    Time: 4 ms (75.69%), Space: 12.2 MB (36.64%) - LeetHub

---
+++ next-permutation/next-permutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        while(next_permutation(nums.begin(), nums.end()))
+        {
+          break;
+        }
+      
+      return;
+    }
+};
