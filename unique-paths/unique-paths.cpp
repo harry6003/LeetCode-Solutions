e9d27d7795eba9f3ce02025edc5446717e0ac268
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-12-05 02:02:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (68.92%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-      
-      int arr[m][n];
-      
-      for(int i=0;i<m;i++)
-      {
-        for(int j=0;j<n;j++)
-        {
-          if(i == 0 || j ==0 )
-            arr[i][j] = 1;
-          else
-            arr[i][j] = arr[i][j-1]+arr[i-1][j];
-        }
-      }
-      
-      return arr[m-1][n-1];
+int uniquePaths(int m, int n) {
+    int N = m+n-2;
+    int r = min(m-1,n-1);
+    long double res = 1.0;
+    for(int i=1; i<=r; i++){
+        res = res*(N-r+i)/i;
     }
+    return (int)res;
+}
 };
