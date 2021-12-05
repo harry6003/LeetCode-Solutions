aec698a24207862fd8e560ad057d96ff7f9b7558
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-12-05 22:53:50 +0530

    Time: 40 ms (84.15%), Space: 13.3 MB (48.60%) - LeetHub

--- 4sum/4sum.cpp
+++ 4sum/4sum.cpp
@@ -2,61 +2,53 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> ans;
-       
-        if(nums.size() < 3)
+        if(nums.size() == 0)
             return ans;
         
-        
         sort(nums.begin(),nums.end());
+        int n = nums.size();
         
-        for(int i=0;i<nums.size()-3;i++)
+        for(int i=0;i<n;i++)
         {
-            if(i == 0 || (i>0 && nums[i] != nums[i-1]))
+            for(int j=i+1;j<n;j++)
             {
-                 for(int j=i+1;j<nums.size()-2;j++)
-                 {
-                    if(j==(i+1) || nums[j]  != nums[j-1])
+                int rem = target -nums[i] - nums[j];
+                
+                int low = j+1;
+                int high = n-1;
+                
+                while(high>low)
+                {
+                 int two_sum = nums[low]+nums[high];
+                    if(two_sum == rem)
                     {
-                        int low = j+1;
-                        int high = nums.size()-1;
-                        int sum = target - (nums[i] + nums[j]);
+                        vector<int> temp(4,0);
+                        temp[0] = nums[i];
+                        temp[1] = nums[j];
+                        temp[2] = nums[low];
+                        temp[3] = nums[high];
+                        ans.push_back(temp);
                         
-                        while(low<high)
-                        {
-                           if(nums[low] + nums[high] == sum)
-                           {
-                                 vector <int> a;
-                                 a.push_back(nums[low]);
-                                 a.push_back(nums[high]);
-                                 a.push_back(nums[i]);
-                                 a.push_back(nums[j]);
-                                 ans.push_back(a);
-                                
-                               
-                               while(low<high && nums[low] == nums[low+1])
-                                   low++;
-                               while(low<high && nums[high] == nums[high-1])
-                                   high--;
-                               
-                               low++;
-                               high--;
-                           }
-                           else if(nums[low] + nums[high] < sum)
-                           {
-                               low++;
-                           }
-                           else 
-                           {
-                               high--;
-                           }
-                        }
+                        while(high>low && nums[low] == nums[low+1])
+                            low++;
+                        while(high>low && nums[high] == nums[high-1])
+                            high--;
+                        
+                        low++;
+                        high--;
                     }
-                 }
-             
+                    else if(two_sum < rem)
+                    {
+                        low++;
+                    }
+                    else
+                        high--;
+                        
+                }
+                 while(j + 1 < n && nums[j + 1] == nums[j]) ++j;
             }
+             while (i + 1 < n && nums[i + 1] == nums[i]) ++i;
         }
-        // for(auto it = s.begin();it != s.end();it++)
-        //     ans.push_back(*it);
         return ans;
     }
 };
