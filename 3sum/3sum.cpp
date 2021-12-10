cb8608fb5a63cbeb9fe7f47fccd236b8557d0c6b
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-12-10 16:38:21 +0530

    Time: 64 ms (84.52%), Space: 21.2 MB (40.88%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-          vector<vector<int>> a; 
-        if(nums.size() == 0 || nums.size() == 1 || nums.size() == 2 )
-            return a;
         
-       sort(nums.begin(),nums.end());
+         vector<vector<int>> ans;
+        if(nums.size() <=2)return ans;
+        
+        sort(nums.begin(),nums.end());
+        
         for(int i=0;i<nums.size()-2;i++)
         {
-            if(i==0 || (i>0 && nums[i] != nums[i-1]))
+            if(i == 0 || nums[i] != nums[i-1])
             {
                 int low = i+1;
                 int high = nums.size()-1;
@@ -16,29 +17,34 @@ public:
                 
                 while(low<high)
                 {
-                    if(nums[low] + nums[high] == sum)
+                    int currsum = nums[low] + nums[high];
+                    if(currsum == sum)
                     {
-                        vector <int> tem;
-                        tem.push_back(nums[i]);
-                        tem.push_back(nums[low]);
-                        tem.push_back(nums[high]);
-                        a.push_back(tem);
+                        vector <int> temp(3);
+                        temp[0] = nums[i];
+                        temp[1]= nums[low];
+                        temp[2]= nums[high];
+                        ans.push_back(temp); 
                         
-                        while(low<high && nums[low] == nums[low+1])
-                            low++;
-                         while(low<high && nums[high] == nums[high-1])
-                             high--;
                         
-                        low++;
+                    while(high>low && nums[low] == nums[low+1])low++;
+                    while(high>low && nums[high] == nums[high-1])high--;
+                    
+                    low++;
+                    high--;
+                    }
+                    else if(currsum>sum)
+                    {
                         high--;
                     }
-                    else if((nums[low] + nums[high] < sum))
-                            low++;
                     else
-                            high--;
+                    {
+                        low++;
+                    }
+                    
                 }  
             }
         }
-        return a;
-    }                          
+        return ans;
+    }
 };
