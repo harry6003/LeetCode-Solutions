2be1862f734d9caa5e672fd359bc50a1ba8fd6a4
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-12-23 13:09:19 +0530

    Time: 4 ms (66.95%), Space: 11.2 MB (79.19%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& v) {
-        int n=v.size();
-        if(n<=1)return 0;
-        vector<int> noStock(n,0) , inHand(n,0) , Sold(n,0);
+    int maxProfit(vector<int>& arr) {
+       vector<vector<int>> dp(2,vector<int> (arr.size()+1));
+ 
+ dp[0][1] = -1*arr[0];
+ dp[1][1] = 0;
+ 
+ for(int i=2;i<=arr.size();i++)
+ {
+    dp[0][i] = max(dp[0][i-1], dp[1][i-2]-arr[i-1]);
+      dp[1][i] = max(dp[0][i-1]+arr[i-1],dp[1][i-1]);
+ }
         
-        noStock[0]=0;
-        inHand[0]=-v[0];
-        Sold[0]=0;
         
-        for(int i=1;i<n;i++){
-            noStock[i]=max(noStock[i-1],Sold[i-1]);
-            inHand[i]=max(inHand[i-1],noStock[i-1]-v[i]);
-            Sold[i]=inHand[i-1]+v[i];
-        }
-        return max(noStock[n-1],Sold[n-1]);
+        return dp[1][arr.size()];
     }
 };
