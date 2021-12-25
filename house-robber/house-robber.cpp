8a032bf12f71ff0c86d385738fa8b798d0037e63
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-12-25 11:24:26 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (20.74%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int sum1 = 0,sum2 = 0;
-        for(int i=0;i<nums.size();i++){
-            if(!(i&1))sum1=max(sum1+nums[i],sum2);
-            else sum2 = max(sum2+nums[i],sum1);
+        int n = nums.size();
+        vector<int> dp(n+1);
+        
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        if(nums.size()<=1)return nums[0];
+      //  int ans  = dp[1];
+        for(int i=2;i<=n;i++)
+        {
+            // dp[i] = dp[i-2]+nums[i-1];
+            if(dp[i-1]>=dp[i-2]+nums[i-1])
+                dp[i] = dp[i-1];
+            else
+                dp[i] = dp[i-2]+nums[i-1];
+           // ans = max(ans,dp[i]);
         }
-        return max(sum1,sum2);
+        // for(int i=0;i<=n;i++)
+        //     cout<<dp[i]<<" ";
+        
+        return dp[n];
     }
 };
