2a2ec3b82777f7e7f5b7eaf94a25cf903838ba63
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2021-12-28 22:30:31 +0530

    Time: 4 ms (35.31%), Space: 7.8 MB (40.57%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -8,18 +8,13 @@ public:
         dp[1] = nums[0];
         
         if(nums.size()<=1)return nums[0];
-      //  int ans  = dp[1];
         for(int i=2;i<=n;i++)
         {
-            // dp[i] = dp[i-2]+nums[i-1];
             if(dp[i-1]>=dp[i-2]+nums[i-1])
                 dp[i] = dp[i-1];
             else
                 dp[i] = dp[i-2]+nums[i-1];
-           // ans = max(ans,dp[i]);
         }
-        // for(int i=0;i<=n;i++)
-        //     cout<<dp[i]<<" ";
         
         return dp[n];
     }
