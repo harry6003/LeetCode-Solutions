8a3c20c5c28a4d58e3b3bb077d514b654bd069c4
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2022-01-10 00:37:31 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -6,18 +6,43 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    int kthElement(int a[], int b[], int n, int m, int k)
-   {
-       int l=min(a[0],b[0]);
-       int h=max(a[n-1],b[m-1]);
-       while(l<=h){
-           int mid=l+(h-l)/2;
-           int x=upper_bound(a,a+n,mid)-a;
-           int y=upper_bound(b,b+m,mid)-b;
-           if(x+y<k) l=mid+1;
-           else h=mid-1;
-       }
-       return l;
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        if(n>m)
+        {
+            return kthElement(arr2,arr1,m,n,k);
+        }
+        
+        int low = max(0,k-m), high = min(k,n);
+        
+        while(low<=high)
+        {
+            int cut1 = (low+high)/2;
+            int cut2 = k - cut1;
+       
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2-1];
+            
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1];
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2];
+            
+       //    cout<<"l1 -> "<<l1<<" l2 -> "<<l2<<" r1 -> "<<r1<<" r2 -> "<<r2<<endl;
+            if(l1<=r2  && l2<=r1)
+            {
+                return max(l2,l1);
+            }
+            else if(l1>r2)
+            {
+                 high = cut1-1;
+            }
+            else if(l2>r1)
+            {
+               low = cut1+1;
+            }
+      
+            
+        }
+        return 1;
    }
 };
 
