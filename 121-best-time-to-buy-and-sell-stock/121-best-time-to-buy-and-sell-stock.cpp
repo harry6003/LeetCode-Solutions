084c96dc765a33b6938d930380919141218df1a4
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2022-03-25 12:10:07 +0530

    Time: 269 ms (8.01%), Space: 93.4 MB (53.56%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& nums) {
+    int maxProfit(vector<int>& prices) {
         
-        int curr_sum = 0;
-        int max_sum = 0;
+       int lsf = INT_MAX; // least so far
+        int op = 0; // overall profit
+        int pist = 0; // profit if sold today
         
-        for(int i=1;i<nums.size();i++)
-        {
-            if(curr_sum + (nums[i]-nums[i-1]) > 0 )
-            {
-                curr_sum += (nums[i]-nums[i-1]);
-                max_sum = max(max_sum, curr_sum);
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < lsf){ // if we found new buy value which is more smaller then previous one
+                lsf = prices[i]; // update our least so far
             }
-            else
-            {
-                curr_sum = 0;
+            pist = prices[i] - lsf; // calculating profit if sold today by, Buy - sell
+            if(op < pist){ // if pist is more then our previous overall profit
+                op = pist; // update overall profit
             }
         }
-        return max_sum;
+        return op; // return op 
         
     }
 };
