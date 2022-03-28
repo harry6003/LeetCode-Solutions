ff3f4e3fdbe071b4397cc0c58fdfe735b9da2d8f
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2022-03-28 11:51:25 +0530

    Time: 1229 ms (5.62%), Space: 50.5 MB (11.65%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     
-    bool solve(vector<int> & nums,int i,vector<int>& dp)
-    {
-        if(i==nums.size()-1)
-        {
-            return dp[i] = true;
-        }
-        if(dp[i]!= -1)return dp[i];
+//     bool solve(vector<int> & nums,int i,vector<int>& dp)
+//     {
+//         if(i==nums.size()-1)
+//         {
+//             return dp[i] = true;
+//         }
+//         if(dp[i]!= -1)return dp[i];
         
-        for(int j=1;j<=nums[i];j++)
-        {
-            if(i+j<nums.size())
-             if(solve(nums,i+j,dp))
-              return dp[i] = true;
-        }
-        return dp[i] = false;
-    }
+//         for(int j=1;j<=nums[i];j++)
+//         {
+//             if(i+j<nums.size())
+//              if(solve(nums,i+j,dp))
+//               return dp[i] = true;
+//         }
+//         return dp[i] = false;
+//     }
         
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        // for(int i=nums.size()-1;i>=0;i--)
-        // {
-        //     bool flag = false;
-        //     for(int j = 0;j<nums[i];j++)
-        //     {
-        //         if()
-        //     }
-        // }
-        return solve(nums,0,dp);
+        
+        if(nums.size()<2)return true;
+        
+        vector<int> dp(nums.size(),false);
+        dp[nums.size()-1] = true;
+        for(int i=nums.size()-2;i>=0;i--)
+        {
+            bool flag = false;
+            for(int j = 1;j<=nums[i];j++)
+            {
+                if(i+j >= nums.size() || dp[i+j] == true)
+                {
+                    flag = true;
+                    break;
+                }  
+            }
+            dp[i] = flag;
+        }
+        return dp[0];
     }
 };
