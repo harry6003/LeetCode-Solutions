ef14cd9f75b30d2ddadbe80e9c7bc0c8c6f91606
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2022-07-29 19:52:50 +0530

    Time: 1000 ms (20.00%), Space: 7.8 MB (91.48%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
-  bool exist(vector<vector<char>>& board, string word) {
-    for (unsigned int i = 0; i < board.size(); i++) 
-        for (unsigned int j = 0; j < board[0].size(); j++) 
-            if (dfs(board, i, j, word))
-                return true;
-    return false;
-}
-
-bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-    if (!word.size())
-        return true;
-    if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
+    
+    bool doesExist(vector<vector<char>>& board, string word,int i,int j,int m,int n)
+    {
+        if(word.size() == 0)
+            return true;
+        
+        if(i>=n || i<0 || j>=m || j< 0 || word[0] != board[i][j])
+            return false;
+        
+        char c = board[i][j];
+        board[i][j] = '*';
+        string s = word.substr(1);
+        bool top = doesExist(board,s,i,j+1,m,n);
+        bool bottom = doesExist(board,s,i,j-1,m,n);
+        bool left = doesExist(board,s,i-1,j,m,n);
+        bool right = doesExist(board,s,i+1,j,m,n);
+        board[i][j] = c;
+        return top || bottom || left || right;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        int n = board.size();
+        int m = board[0].size();
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(doesExist(board,word,i,j,m,n))
+                    return true;
+            }
+        }
         return false;
-    char c = board[i][j];
-    board[i][j] = '*';
-    string s = word.substr(1);
-    bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
-    board[i][j] = c;
-    return ret;
-}
-      
+    }
 };
