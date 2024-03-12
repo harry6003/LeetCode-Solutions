ddf6f95bd70d10cbbdb3695b73554247b0de8915
Himanshu Agrawal <72691698+harry6003@users.noreply.github.com>
2024-03-13 02:24:13 +0530

    Update link_tickets.py

--- link_tickets.py
+++ link_tickets.py
@@ -7,9 +7,9 @@ def extract_ticket_ids(pr_description):
 
 def generate_hyperlink(ticket_id):
     return f'[Jira Ticket {ticket_id}](https://mobilerndhub.com/BHP/{ticket_id})'
-
+    
 if __name__ == "__main__":
-    pr_description = input()  # Read PR description from standard input
+    pr_description = sys.argv[1]  # Extract PR description from command-line arguments
     ticket_ids = extract_ticket_ids(pr_description)
     for ticket_id in ticket_ids:
         hyperlink = generate_hyperlink(ticket_id)
